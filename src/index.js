4fba3512690192f95467e6249d2d02ad9ad92178
ken <block24block@gmail.com>
2021-03-31 17:37:56 +0800

    hot reload

--- src/index.js
+++ src/index.js
@@ -42,5 +42,9 @@ app.on('activate', () => {
   }
 });
 
+try {
+  require('electron-reloader')(module);
+} catch {}
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
