ef42ff637a4619eaa2ae61e50f1cb0264cb2ee8e
ken <block24block@gmail.com>
2021-04-09 15:07:26 +0800

    move calendar callbacks definition into its module

--- src/rendering.js
+++ src/rendering.js
@@ -47,65 +47,7 @@
     return `${e.start}|${e.title}`
   }
 
-  // 拼接对话框中的日期
-  function convertLabel(info) {
-    let deadline = new Date(info.end.getTime() - 24 * 60 * 60 * 1000)
-    if (info.start.toString() === deadline.toString()) {
-      return info.startStr
-    }
-    return `${info.startStr} ~ ${deadline.getFullYear()}-${deadline.getMonth() + 1}-${deadline.getDate()}`
-  }
-
-  // 选定日期后回调
-  function selectDate(info) {
-    let label = convertLabel(info)
-
-    prompt({
-      title: 'add event',
-      label: label,
-      type: 'input',
-      height: 200,
-    })
-      .then((eventName) => {
-        if (eventName === null || eventName === '' || eventName === undefined) {
-          return
-        }
-        let event = {
-          title: eventName,
-          start: info.startStr,
-          end: info.endStr,
-        }
-        db.addEvent(event)
-        event.id = eventId(event)
-        calendar.addEvent(event)
-      })
-      .catch(console.error)
-  }
-
-  // 选定事情后回调
-  function clickEvent(info) {
-    let event = info.event
-    prompt({
-      title: 'remove event',
-      label: convertLabel(event) + ' : ' + event.title,
-      type: 'input',
-      height: 200,
-    })
-      .then((confirmation) => {
-        if (confirmation === 'y') {
-          event.remove()
-          db.removeEvent(event)
-        }
-      })
-      .catch(console.error)
-  }
-
-  // 移动、伸缩事情后回调
-  function moveEvent(info) {
-    db.removeEvent(info.oldEvent)
-    db.addEvent(info.event)
-  }
-
+  // 渲染日期
   function datesSet(info) {
     let now = calendar.getDate(),
       month = now.getMonth() + 1,
@@ -117,6 +59,67 @@
   // 创建日历
   let calendar
   function createCalendar(jsonData) {
+    // 拼接对话框中的日期
+    function convertLabel(info) {
+      let deadline = new Date(info.end.getTime() - 24 * 60 * 60 * 1000)
+      if (info.start.toString() === deadline.toString()) {
+        return info.startStr
+      }
+      return `${info.startStr} ~ ${deadline.getFullYear()}-${deadline.getMonth() + 1}-${deadline.getDate()}`
+    }
+
+    // 选定日期后回调
+    function selectDate(info) {
+      let label = convertLabel(info)
+
+      prompt({
+        title: 'add event',
+        label: label,
+        type: 'input',
+        height: 200,
+      })
+        .then((eventName) => {
+          if (eventName === null || eventName === '' || eventName === undefined) {
+            return
+          }
+          let event = {
+            title: eventName,
+            start: info.startStr,
+            end: info.endStr,
+          }
+          db.addEvent(event)
+          event.id = eventId(event)
+          calendar.addEvent(event)
+        })
+        .catch(console.error)
+    }
+
+    // 选定事情后回调
+    function clickEvent(info) {
+      let event = info.event
+      prompt({
+        title: 'remove event',
+        label: convertLabel(event) + ' : ' + event.title,
+        type: 'input',
+        height: 200,
+      })
+        .then((confirmation) => {
+          if (confirmation === 'y') {
+            event.remove()
+            db.removeEvent(event)
+          }
+        })
+        .catch(console.error)
+    }
+
+    // 移动、伸缩事情后回调
+    function moveEvent(info) {
+      db.removeEvent(info.oldEvent)
+      db.addEvent(info.event)
+    }
+
+    let buttons = ['prev', 'today', 'next']
+
     calendar = new FullCalendar.Calendar(document.getElementById('calendar'), {
       initialView: 'dayGridMonth',
       headerToolbar: false,
@@ -136,7 +139,7 @@
     })
     calendar.render()
     // 跳转日期
-    ;['prev', 'today', 'next'].forEach((action) => {
+    buttons.forEach((action) => {
       document.getElementById('go' + action).addEventListener('click', () => {
         console.log(action)
         calendar[action]()
@@ -158,7 +161,7 @@
       selectable: true,
       editable: true,
       droppable: true,
-      select: selectDate,
+      // select: selectDate,
     })
     pending.render()
     pending.gotoDate('2999-01-01')
